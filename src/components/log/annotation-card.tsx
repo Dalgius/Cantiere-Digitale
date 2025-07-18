e1cee512299da038ca93dfa6325ecba5bfa7bb85
giuseppe d'alessio <dalgius70@gmail.com>
2025-07-18 13:53:40 +0000

    togli la nuvoletta grigia dietro "Descrizione lavori svolti"

--- src/components/log/annotation-card.tsx
+++ src/components/log/annotation-card.tsx
@@ -24,7 +24,7 @@ const getBadgeVariant = (type: Annotation['type']) => {
     case 'Istruzioni / Ordine di Servizio':
       return 'default';
     default:
-      return 'secondary';
+      return 'outline';
   }
 };
 
