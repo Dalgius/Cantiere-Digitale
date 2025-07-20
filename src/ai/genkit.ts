0ea8ec3ea2b38e7e473b238bea80ebdb77e61c17
giuseppe d'alessio <dalgius70@gmail.com>
2025-07-20 17:35:41 +0000

    ho dettato il testo e l'ha trascritto correttamente, ho poi voluto usare

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,19 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      safetySettings: [
+        {
+          category: 'HARM_CATEGORY_HATE_SPEECH',
+          threshold: 'BLOCK_ONLY_HIGH',
+        },
+        {
+          category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+          threshold: 'BLOCK_ONLY_HIGH',
+        },
+      ],
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
