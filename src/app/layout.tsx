71f85e3ec596c9b52f0c0261b0d934fa1aa292a1
giuseppe d'alessio <dalgius70@gmail.com>
2025-07-24 14:59:23 +0000

    vedo l'icona di firebase sullo smartphone, non quella che ho caricato

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: "Cantiere Digitale",
   description: "La versione moderna e definitiva del Giornale dei Lavori.",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -18,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="it" suppressHydrationWarning>
       <head>
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#6699CC" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
