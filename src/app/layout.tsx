e97be39fcebcc9a38f0019f6462e01e80f8d40e6
giuseppe d'alessio <dalgius70@gmail.com>
2025-07-24 16:00:13 +0000

    ancora  niente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,9 @@ export default function RootLayout({
     <html lang="it" suppressHydrationWarning>
       <head>
         <meta name="theme-color" content="#6699CC" />
-        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
